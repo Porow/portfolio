072300965f6f2c08230bba9fa8c5c3a64058d245
unknown <poro@42.fr>
2018-05-26 04:17:44 +0200

    Pushing right directory and fixed 404 background

--- src/locales.js
+++ src/locales.js
@@ -18,20 +18,34 @@ export const translations = {
     NotFound: ['Not Found', 'Page introuvable'],
     NotFoundText: ['Sorry, but it seems that this page doesn\'t exist in this dimension. <br/> You should try <a className="nav-item" href="/">this one.</a>',
         'Navré, il semblerait que cette page n\'existe pas dans cette dimension. <br/> Vous devriez essayer <a className="nav-item" href="/">celle-ci.</a>'],
-    aboutme: ['Fuck you', 'Je suis un développeur front-end que les difficultés n\'arrêtent pas. <br/>' +
-    'Mon goût du challenge me pousse à aller toujours plus loin dans mon apprentissage permanent des diverses technologies populaires en l\'instant. <br/>' +
-    'J\'aime débattre avec mes pairs pour savoir laquelle de ces technologies est la plus efficace pour mon besoin actuel. <br/>' +
-    'J\'ai ainsi pu apprendre le PHP, HTML, CSS, JavaScript, NodeJS, ReactJS, l\'utilisation de MySQL, MongoDB, de framework tel que Symfony... <br/>' +
-    '<br/>' +
-    'Ayant commencé sur Dreamwaver CS3 à 10 ans (on fait parfois des erreurs de jeunesse), le Web m\'a toujours passionné.<br/>' +
-    'J\'aime produire rapidement du contenu et le faire évoluer constamment.<br/>' +
-    'L\'apprentissage du C au cours de ma formation à 42 a contribué à solidifier et renforcer mes bases, ma logique et ma résolution de problèmes.<br/>' +
-    'J\'y ai également appris à travailler en équipe et l\'utilisation de git.<br/>' +
-    '<br/>' +
-    'Quelques informations complémentaires :<br/>' +
-    '- Je suis un fan inconditionnel (ou presque) de Star Wars.<br/>' +
-    '- Je joue du piano depuis 2 ans<br/>' +
-    '- J\'ai été auteur à partir de mes 8 ans jusqu\'à mes 16 ans<br/>' +
-    '- J\'aime ma voiture (peut-être trop)<br/>' +
-    '- La plupart des objets connectés sont des reliques à mes yeux']
+    aboutme: ['I\'m a front-end developer that problems won\'t stop <br/>'
+    + 'My competition mind makes me going further each time to learn the most used languages.<br/>'
+    + 'I love discussing with anybody to see which language is the best for my current project. <br/>'
+    + 'That\'s how I came up learning PHP, HTML, CSS, Javascript, NodeJS, ReactJs, MySQL, MongoDB, Symfony and others. <br/>'
+    + 'I started on Dreamweaver CS3 at 10 years old (yeah, I made some mistakes in my early age). Web fascinated me already. <br/>'
+    + 'I love to build and keep updating content. <br/>'
+    + 'I learned C at 42, which gave me stronger knowledge, logic and helped me solving problems. <br/>'
+    + 'I got familiarized with git and peer working.<br/>'
+    + 'Some random facts about me :<br/>'
+    + '- I just love Star Wars. Like... I really do. Or Do not. But there is not try.<br/>'
+    + '- I\'ve been playing the piano for 2 years now<br/>'
+    + '- From my 8 years old to my 16, I wrote 6 books that\'ve been published.<br/>'
+    + '- My car is my true love.<br/>'
+    + '- Almost every connected or smart object is almost holly to me.<br/>',
+        'Je suis un développeur front-end que les difficultés n\'arrêtent pas. <br/>'
+        + 'Mon goût du challenge me pousse à aller toujours plus loin dans mon apprentissage permanent des diverses technologies populaires en l\'instant. <br/>'
+        + 'J\'aime débattre avec mes pairs pour savoir laquelle de ces technologies est la plus efficace pour mon besoin actuel. <br/>'
+        + 'J\'ai ainsi pu apprendre le PHP, HTML, CSS, JavaScript, NodeJS, ReactJS, l\'utilisation de MySQL, MongoDB, de framework tel que Symfony... <br/>'
+        + '<br/>'
+        + 'Ayant commencé sur Dreamwaver CS3 à 10 ans (on fait parfois des erreurs de jeunesse), le Web m\'a toujours passionné.<br/>'
+        + 'J\'aime produire rapidement du contenu et le faire évoluer constamment.<br/>'
+        + 'L\'apprentissage du C au cours de ma formation à 42 a contribué à solidifier et renforcer mes bases, ma logique et ma résolution de problèmes.<br/>'
+        + 'J\'y ai également appris à travailler en équipe et l\'utilisation de git.<br/>'
+        + '<br/>'
+        + 'Quelques informations complémentaires :<br/>'
+        + '- Je suis un fan inconditionnel (ou presque) de Star Wars.<br/>'
+        + '- Je joue du piano depuis 2 ans<br/>'
+        + '- J\'ai été auteur à partir de mes 8 ans jusqu\'à mes 16 ans<br/>'
+        + '- J\'aime ma voiture (peut-être trop)<br/>'
+        + '- La plupart des objets connectés sont des reliques à mes yeux']
 };
